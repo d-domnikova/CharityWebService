c67b12cb2d70ecd525bac736dcb8f690b4f71986
chntk <dddomnikova@gmail.com>
2024-06-18 22:12:27 +0300

    [1] Initial commit. Created project with ttcli console application.

---
+++ src/main/java/edu/domnikova/CharityWebService/CharityWebServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.domnikova.CharityWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharityWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharityWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/domnikova/CharityWebService/CharityWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.domnikova.CharityWebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharityWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
